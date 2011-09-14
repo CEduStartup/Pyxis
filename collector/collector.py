240a57d9f69d7ab0add2f434b47d4021d9307eb7
Evgen Koval <ekoval@ironport-training.lv.lohika.com>
2011-09-14 15:27:05 +0300

    First prototype of storage subsystem

--- collector/collector.py
+++ collector/collector.py
@@ -1,11 +1,18 @@
 import gevent
+import settings
+
 
 from Scheduler import Scheduler
 from TrackerCollection import TrackerCollection
+from storage import storage_types
 
+storage_class = storage_types[settings.STORAGE_TYPE]
+storage = storage_class(settings.STORAGE_HOST, settings.STORAGE_PORT,
+                        settings.STORAGE_DB_NAME)
 scheduler = Scheduler()
-trackerCollection = TrackerCollection(scheduler)
+trackerCollection = TrackerCollection(scheduler, storage)
 
+storage.start()
 scheduler.start()
 
 # Main thread just refreshing config
