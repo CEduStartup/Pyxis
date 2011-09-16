7950b6603be1ef6f4c16b11283b2c07f8952551f
Yuriy Konovalyuk <stratocoder@gmail.com>
2011-09-16 12:52:28 +0300

    Revert "Backdoor added. Command line arguments handling improved."
    This reverts commit 7a51056c2e7b861d2274919904b00bb5f59c9997.

--- collector/collector.py
+++ collector/collector.py
@@ -3,7 +3,6 @@ import settings
 import sys
 import time
 
-from gevent.backdoor import BackdoorServer
 from Scheduler import Scheduler
 from TrackerCollection import TrackerCollection
 from storage import storage_types
@@ -18,36 +17,15 @@ trackerCollection = TrackerCollection(scheduler, storage)
 storage.start()
 scheduler.start()
 
-def handle_command_line_args():
-    MONITOR = '--monitor'
-    BACKD = '--backdoor'
-    HELP = '--help'
+if '--monitor' in sys.argv:
+    def monitor_qsize():
+        while True:
+            print time.strftime('%H:%M:%S')
+            print '\tScheduler:', scheduler.get_run_queue_size()
+            print '\tStorage:  ', storage.get_store_queue_size()
+            gevent.sleep(2)
 
-    if MONITOR in sys.argv:
-        def monitor_qsize():
-            while True:
-                print time.strftime('%H:%M:%S')
-                print '\tScheduler:', scheduler.get_run_queue_size()
-                print '\tStorage:  ', storage.get_store_queue_size()
-                gevent.sleep(int(sys.argv[sys.argv.index(MONITOR) + 1]))
-
-        gevent.spawn(monitor_qsize)
-
-    if BACKD in sys.argv:
-        def start_backdoor():
-            BackdoorServer(('127.0.0.1', int(sys.argv[sys.argv.index(BACKD) + 1]))).serve_forever()
-        gevent.spawn(start_backdoor)
-
-    if HELP in sys.argv:
-        print """\
-Options:
---help - print this message without running app.
---monitor SECONDS - print scheduler and storage queue with SECONDS interval.
---backdoor PORT - start backdoor server listening to PORT on localhost.
-"""
-        exit()
-
-handle_command_line_args()
+    gevent.spawn(monitor_qsize)
 
 # Main thread just refreshing config
 while True:
