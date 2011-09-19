5b99afe0d6506592e714431a474f78e19f6568ce
Alex <alex@localhost>
2011-09-19 20:20:20 +0300

    Added base forms

--- admingui/frontend/views/trackers.py
+++ admingui/frontend/views/trackers.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render_to_response
+from ..forms import TrackerForm
 
 def index(request):
     return render_to_response('frontend/trackers/index.html')
@@ -7,7 +8,15 @@ def view(request):
     return index(request)
 
 def add(request):
-    return index(request)
+    if request.method == 'POST':
+        form = TrackerForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = TrackerForm()
+    return render_to_response('frontend/trackers/form.html', {
+        'form': form
+    })
 
 def edit(request):
     return index(request)
