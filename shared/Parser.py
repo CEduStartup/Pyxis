ffb8b63c2b5a7d92d14f155a73835cfa3a0a4999
Yuriy <yura.senko@gmail.com>
2011-09-22 10:13:15 +0300

    deleted mistakenly created file.

--- shared/Parser.py
+++ shared/Parser.py
@@ -1,4 +1,4 @@
-"""This module contains different data parsers compatible with `gevent`.
+"""This module contains different data parsers.
 
 Don't create parser instance manually, rather use `get_parser()` factory
 method.
@@ -9,10 +9,12 @@ import BeautifulSoup
 import gevent
 import StringIO
 
-import target
-
-from lxml import etree, html
+from lxml import etree
 from lxml.html import soupparser
+
+
+# Determine how many tags will be parser before return control to gevent()
+ELEMENTS_IN_ROUND = 50
 
 
 class ParserError(Exception):
@@ -20,17 +22,67 @@ class ParserError(Exception):
     """Base class for all parser errors.
     """
 
+
+class ParserNotInitializedError(ParserError):
+
+    """Operation with not initialized parser.
+    """
+
+
 class ParserSyntaxError(ParserError):
 
     """This class describes all syntax errors which can occur during data
     parsing.
 
+    print p1.xpath('//TEMPERATURE/@max')
     Please use `description` attribute to get error details.
     """
 
     def __init__(self, details=''):
         ParserError.__init__(self),
         self.description = details
+
+
+class GTreeBuilder(etree.TreeBuilder):
+
+    """This class do the same work as `etree.TreeBuilder` but it's compatible
+    with gevent.
+    """
+
+    def __init__(self, max_elements=ELEMENTS_IN_ROUND):
+        etree.TreeBuilder.__init__(self)
+        self._elements_in_round = max_elements
+        self._elements_parsed = 0
+
+    def start(self, tag, attrs):
+        if self._elements_parsed == self._elements_in_round:
+            self._elements_parsed = 0
+            gevent.sleep(0)
+
+        self._elements_parsed += 1
+        return etree.TreeBuilder.start(self, tag, attrs)
+
+
+def _check_initialization(method):
+    """Check if the parser is initialized.
+
+    :Return:
+        - `None`
+
+    :Exception:
+        - `ParserNotInitializedError`: in case when `._parser` attribute is
+          `None`.
+    """
+    def check(self, *args, **kwargs):
+        """Inner check method. Check if the parser is initialize and if not
+        then raise execption.
+        """
+        if self._parser is None:
+            raise ParserNotInitializedError()
+        return method(self, *args, **kwargs)
+
+    return check
+
 
 class BaseParser(object):
 
@@ -42,10 +94,17 @@ class BaseParser(object):
     def __init__(self):
         self._parser = None
 
-    @abc.abstractmethod
     def initialize(self):
         """Create and initialize parser.
         """
+        self._create_parser()
+        self._initialize()
+
+    def _initialize(self):
+        """Responsible for custom parser initialization. You can overwrite
+        this method in child class.
+        """
+        pass
 
     @abc.abstractmethod
     def parse(self, raw_data):
@@ -59,6 +118,19 @@ class BaseParser(object):
             - `ParserSyntaxError`: in case when `raw_data` cannot be parsed
               because of critical syntax error.
         """
+
+    @abc.abstractmethod
+    def _create_parser(self):
+        """This method responsible for parser creation. You need to overwrite
+        this method in your class.
+        """
+        self._parser = None
+
+    @abc.abstractmethod
+    def _parse(self, raw_data):
+        """Inner implementation of the parser.
+        """
+
 
 class GBeautifulSoupParser(BeautifulSoup.BeautifulSoup):
 
@@ -74,7 +146,7 @@ class GBeautifulSoupParser(BeautifulSoup.BeautifulSoup):
         BeautifulSoup.BeautifulSoup.__init__(self, *args, **kwargs)
 
     # How many tags will be parsed before switching control to gevent.
-    TAGS_IN_ROUND = 50
+    TAGS_IN_ROUND = ELEMENTS_IN_ROUND
 
     def parse_starttag(self, start_pos):
         """Handle `starttag` event.
@@ -91,17 +163,17 @@ class GBeautifulSoupParser(BeautifulSoup.BeautifulSoup):
 
 class XMLParser(BaseParser):
 
-    """XML parser compatible with `gevent`.
+    """XML parser which is not compatible with gevent.
     """
 
     def __init__(self):
         BaseParser.__init__(self)
         self._etree_dom = None
 
-    def initialize(self):
-        """Prepare parser to work.
+    def _create_parser(self):
+        """Create XML parser.
         """
-        xml_target = target.geventTreeBuilder()
+        xml_target = etree.TreeBuilder()
         self._parser = etree.XMLParser(target=xml_target)
 
     def _parse(self, data):
@@ -119,7 +191,7 @@ class XMLParser(BaseParser):
         """
         return etree.parse(data, self._parser)
 
-
+    @_check_initialization
     def parse(self, raw_data):
         """Parse `raw_data` and return XML DOM compatible with `lxml.etree`.
         """
@@ -151,41 +223,84 @@ class XMLParser(BaseParser):
         return data
 
 
+class GXMLParser(XMLParser):
+
+    """XML parser compatible with `gevent`.
+    """
+
+    def _create_parser(self):
+        """Prepare parser to work.
+        """
+        xml_target = target.GTreeBuilder()
+        self._parser = etree.XMLParser(target=xml_target)
+
+
 class HTMLParser(XMLParser):
 
     """HTML parser.
-
-    Don't create it directly use `get_parser()` factory method.
     """
 
-    def initialize(self):
-        self._parser = GBeautifulSoupParser
+    def _create_parser(self):
+        self._parser = BeautifulSoup.BeautifulSoup
 
     def _parse(self, data):
-        """Please see description of the method in parent class: `XMLParser`.
+        """Parse the document `data` and return result (`ElementTree` object).
         """
-        return html.soupparser.parse(data, beautifulsoup=self._parser)
+        return soupparser.parse(data, beautifulsoup=self._parser)
+
+    def parse(self, data):
+        """High level parse method. Parse document, handle errors. Assign
+        parsing result (`lxml.etree.ElementTree` object) to `_etree_dom`
+        attribute.
+        """
+        # TODO: Add errors handling.
+        self._etree_dom = self._parse(StringIO.StringIO(data))
+
+
+class GHTMLParser(HTMLParser):
+
+    """HTML parser.
+    """
+
+    def _create_parser(self):
+        self._parser = GBeautifulSoupParser
 
 
 # Maps datatype to parser class which can handle it.
 _PARSER_TYPES_MAPPING = {
-    'xml': XMLParser,
-    'html': HTMLParser,
+    'plain': {
+        'xml': XMLParser,
+        'html': HTMLParser,
+    },
+
+    'gevent_safe': {
+        'xml': GXMLParser,
+        'html': GHTMLParser,
+    }
 }
 
-def get_parser(datatype='xml'):
+def get_parser(datatype='xml', gevent_safe=True):
     """Return `lxml` compatible parsers.
 
     :Parameters:
         - `datatype`: string which contains datatype. Currently it's one of the
           following: 'xml', 'html', 'json'.
-    """
-    return _PARSER_TYPES_MAPPING[datatype]()
+        - `gevent_safe`: boolean. If `True` then this method will *always*
+          return parser instance which is compatible with gevent.
 
+    :Return:
+        - An instance of a parser compatible with the given `datatype`.
+    """
+    threads_type = 'gevent_safe'
+    if not gevent_safe:
+        threads_type = 'plain'
+
+    return _PARSER_TYPES_MAPPING[threads_type][datatype]()
 
 if __name__ == '__main__':
-    p = get_parser('html')
+    d = open('t/xml.xml')
+    p = get_parser('xml', gevent_safe=False)
     p.initialize()
-    data = ' '.join(open('index.html').readlines())
-    p.parse(data)
-    print p._etree_dom
+    p.parse(' '.join(d.readlines()))
+    d.close()
+    print p.xpath('//TEMPERATURE/@max')
