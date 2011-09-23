8c45847d2a56abe8abc51dcf760700dd618102af
Yuriy Konovalyuk <stratocoder@gmail.com>
2011-09-23 12:44:41 +0300

    Events added

---
+++ shared/events/TrackerEvents.py
@@ -0,0 +1,47 @@
+""" Tracker events definitions.
+"""
+
+from Event import BaseEvent
+
+
+class TrackerEvent(BaseEvent):
+
+    """ Base class for all tracker events.
+
+    Don't invoke this event.
+    """
+
+    tracker_id = None
+
+
+class TrackerSuccessEvent(TrackerEvent):
+    
+    """ Invoked when tracker succesfully grabbed data.
+    """
+
+    self.eid = 'TRACKER.SUCCESS'
+    
+    self.message = 'Tracker %s succesfully grabbed data.'
+
+
+class TrackerFailureEvent(TrackerEvent):
+
+    """ Base class for all tracker failures events.
+    """
+
+
+class TrackerParseErrorEvent(TrackerFailureEvent):
+    
+    """ Invoked when parser error occure during data grabbing.
+    """
+
+    self.eid = 'TRACKER.FAILURE.PARSE'
+
+    self.message = ''
+
+
+class TrackerWorkflowEvent(TrackerEvent):
+
+    """ Base class for all non-failure events during data grabbing.
+    """
+
