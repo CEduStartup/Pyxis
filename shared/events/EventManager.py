bca499694430d7716b77eb62d4130802793f2c51
Yuriy Konovalyuk <stratocoder@gmail.com>
2011-09-27 14:24:00 +0300

    Receiver updated.

--- shared/events/EventManager.py
+++ shared/events/EventManager.py
@@ -1,14 +1,21 @@
-import beanstalkc
 import pickle
 
 
 class EventReceiver(EventManagerBase):
+
+    """ Used to receive messages from a single tube.
+    This class is non thread safe.
+    """
 
     def __init__(self, server_host, server_port, tube, callback):
         EventReceiverBase.__init__(self, server_host, server_port, tube)
         self._callback = callback
 
     def dispatch(self):
+        """ Method that receive from message queue, restore and throw events to
+        subscribed callback.
+        """
+
         while True:
             job = self._client.reserve()
             event = pickle.loads(job.body)
