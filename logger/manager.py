eef3e083521215d692662914dd3a00fc2c42611e
Yuriy <yura.senko@gmail.com>
2011-10-04 11:41:59 +0300

    Now logger handles all events derived from BaseEvent class.

--- logger/manager.py
+++ logger/manager.py
@@ -1,33 +1,33 @@
 from shared.events.EventManager import EventReceiver
 from datetime import datetime
-from shared.events.Event import LoggerEvent, LOGGER_TUBE
+from shared.events.Event import BaseEvent, LOGGER_TUBE
 from config.mq import QUEUE_HOST, QUEUE_PORT
 
 class LogManager:
-    
-    """ Log manager which listens for logger events and shows them on the screen. 
-    
+
+    """ Log manager which listens for logger events and shows them on the screen.
+
     Now provides possibility just to show all logs.
-    FUTURE: possibility to filter by some rules        
+    FUTURE: possibility to filter by some rules
     """
-    
+
     def __init__(self, queue_host, queue_port):
         self.receiver = EventReceiver(server_host=queue_host,
                                       server_port=queue_port,
                                       tubes=(LOGGER_TUBE,),
                                       callback=self.on_message)
-    
+
     def start(self):
         self.receiver.dispatch()
-        
+
     def on_message(self, event):
-        if isinstance(event, LoggerEvent):
+        if isinstance(event, BaseEvent):
             log_msg = '%s [ %s ] %s' % (datetime.fromtimestamp(event.time).strftime('%Y-%m-%d %H:%M:%S'),
                                         event.level.upper(),
                                         event.msg)
             print log_msg
-    
-    
+
+
 if __name__ == '__main__':
     LogManager(QUEUE_HOST, QUEUE_PORT).start()
 
