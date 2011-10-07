8d7d4be97d97786ff1f1a6d2e8f39d8e6f9879f0
Yuriy Konovalyuk <stratocoder@gmail.com>
2011-10-07 12:17:29 +0300

    First approach for make/run script is done.

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+#
+# Bash script for starting and stopping pyxis.
+#
+# Usage:
+# ./run.sh command
+#  - command - one of start, stop, status
+#
 . ./comp.conf
 
-export PYTHONPATH=`python ~/work/pyxis/pyxis/build_python_path.py ~/work/pyxis/pyxis/`:$PYTHONPATH
+export PYTHONPATH=`python ./build_python_path.py ./`:$PYTHONPATH
 
+# Function for checking whether some component is running or not.
+# It matches component process among other processes by {component_name}_match_string from comp.conf file.
+# Takes a single argument - component name.
 get_running () {
     item=$1
     matchstr=$(eval "echo \${$(echo ${item}_match_string)}")
@@ -24,6 +34,8 @@ get_running () {
     echo $pid
 }
 
+# Function that echoes to console whether some component is running or not.
+# Takes a single argument - component name.
 show_running () {
     running=`get_running $1`
     if [ $running -gt 0 ]
@@ -34,6 +46,8 @@ show_running () {
     fi
 }
 
+# Function for starting components. It is uses {component_name}_start from comp.conf file for running components.
+# Takes a single argument - component name.
 start_process () {
     program=$1
     echo "Starting $program..."
@@ -41,6 +55,8 @@ start_process () {
     echo $! > $program.pid
 }
 
+# Function for stopping components. It just kills components processes by PID.
+# Takes a single argument - component name.
 stop_process () {
     program=$1
     pid=`get_running $1`
@@ -52,6 +68,8 @@ stop_process () {
     fi
 }
 
+# Applies some function for all components.
+# Takes a single argument - function name.
 for_each () {
     for server in "${servers[@]}"
     do
@@ -69,7 +87,9 @@ for_each () {
 
 if [ $# -eq 0 ]
 then
-    for_each show_running
+    echo "Usage:"
+    echo "run.sh command"
+    echo " - command - one of start, stop, status"
 else
     case "$1" in
     start)
@@ -78,9 +98,11 @@ else
     stop)
         for_each stop_process
         ;;
+    status)
+        for_each show_running
+        ;;
     *)
         echo "Bad command"
         ;;
     esac
 fi
-exit
