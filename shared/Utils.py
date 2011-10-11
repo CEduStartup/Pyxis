be08cc052afab20a4097d7e9b844f62f7bcafc28
Maksym Reva <mreva@lohika.com>
2011-10-11 15:53:04 +0300

    Mongodb operations moved to the util class.

--- shared/Utils.py
+++ shared/Utils.py
@@ -23,15 +23,33 @@ def value_generator(n):
         n = randint(f, t)
         yield n
 
+def time_round(ts, period='1day'):
+    d = None
+    if period == '1day':
+        d = ONE_DAY
+    elif period == '1hour':
+        d = ONE_HOUR
+    elif period == '5min':
+        d = FIVE_MIN
+    else:
+        raise ValueError('`period` must be `1day`, `1hour` or `5min`')
+    return d * (int(ts) / d)
+
+def str2time(string):
+    try:
+        return time.mktime(time.strptime(string, '%Y-%m-%d'))
+    except:
+        raise ValueError('Date `%s` does not match format `%Y-%m-%d`.')
+
 def get_date_str_1day(ts):
     return time.strftime('%Y-%m-%d', time.localtime(ts))
 
 def get_date_str_1hour(ts):
-    t = ONE_HOUR * (int(ts) / ONE_HOUR)
+    t = time_round(ts, '1hour')
     return time.strftime('%Y-%m-%d %H', time.localtime(t))
 
 def get_date_str_5min(ts):
-    t = FIVE_MIN * (int(ts) / FIVE_MIN)
+    t = time_round(ts, '5min')
     return time.strftime('%Y-%m-%d %H:%M', time.localtime(t))
 
 date_str_functions = {
@@ -42,4 +60,3 @@ date_str_functions = {
 
 def get_date_str(rollup_period, timestamp):
     return date_str_functions[rollup_period](timestamp)
-
--- shared/db/mongo.py
+++ shared/db/mongo.py
@@ -1,12 +1,14 @@
-from shared.Utils import rollup_periods, get_date_str
+from shared.Utils import rollup_periods, get_date_str, value_generator, \
+                         time_round, str2time, FIVE_MIN, ONE_HOUR, ONE_DAY
+import time
 
 
 class TimeBasedData(Object):
     connection = None
     db = None
 
-    def test_data(self, trackers_count=5, xpaths_per_tracker=3,
-                  date_from=None, date_to=None, duration_in_days='365'):
+    def fill_test_data(self, trackers_count=5, xpaths_per_tracker=3,
+                       date_from=None, date_to=None, duration_in_days=365):
         """Fill in the MongoDb with the test data.
 
         :Parameters:
@@ -23,6 +25,30 @@ class TimeBasedData(Object):
                used with either `date_from` or `date_to`. If both `date_from`
                and `date_to` are specified `duration_in_days` will be omitted.
         """
+        ts_from = ts_to = None
+        if not duration_in_days:
+            duration_in_days = 365
+        if date_from:
+            ts_from = time_round(str2time(date_form))
+        if date_to:
+            ts_to = min(int(time.time()), time_round(str2time(date_to)) + ONE_DAY)
+        if date_from is None and date_to is None:
+            ts_to = int(time.time())
+            ts_from = time_round(ts_to - duration_in_days * ONE_DAY)
+        if ts_from is None:
+            ts_from = time_round(ts_to - duration_in_days * ONE_DAY)
+        if ts_to is None:
+            ts_to = min(int(time.time()), ts_from + duration_in_days * ONE_DAY)
+
+        vg = value_generator(5)
+
+        for ts in range(ts_from, ts_to, FIVE_MIN):
+            for tracker_id in range(1, trackers_count+1):
+                values = {}
+                for i in range(1, xpaths_per_tracker+1):
+                    xpath_id = '%s_%s' % (tracker_id, i)
+                    values[xpath_id] = vg.next()
+                self.insert_raw_data(tracker_id, timestamp, values)
 
     def insert_raw_data(self, tracker_id, timestamp, data):
         """Inserts `data` to the MongoDB and aggregates data in `1hour` and
@@ -42,6 +68,24 @@ class TimeBasedData(Object):
             date = get_date_str(period_name, timestamp)
             res = collection.find_one({'date': date, 'tracker_id': tracker_id})
             if res is None:
-                res = {'tracker_id': tracker_id, 'date': date, 'min': value,
-                       'max': value, 'sum': value, 'count': 1}
+                values = {}
+                res = {'tracker_id': tracker_id, 'date': date, 'values': values}
+                for xpath_id, value in data.iteritems():
+                    values[xpath_id] = {'min': value, 'max': value,
+                                        'sum': value, 'count': 1}
+                collection.save(res)
+            else:
+                values = res['values']
+                for xpath_id, value in data.iteritems():
+                    if not xpath_id in values:
+                        values[xpath_id] = {'min': value, 'max': value,
+                                            'sum': value, 'count': 1}
+                    else:
+                        values[xpath_id]['sum'] += value
+                        values[xpath_id]['count'] += 1
+                        if values[xpath_id]['min'] > value:
+                            values[xpath_id]['min'] = value
+                        if values[xpath_id]['max'] < value:
+                            values[xpath_id]['max'] = value
+                collection.update({'_id': res['_id']}, res, True)
 
