74c6761300e008497f595ad7e422679c0233062b
Sasha B <obystrikov@ironport-training.lv.lohika.com>
2011-10-19 18:20:34 +0300

    Updated trackers and tracker wizard to new model

--- webui/frontend/views/trackers.py
+++ webui/frontend/views/trackers.py
@@ -6,8 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
-from ..models import TrackerModel
-from ..forms import OptionsForm, TrackerForm
+from frontend.models import TrackerModel
+from frontend.forms import OptionsForm, TrackerForm
 from webui.util import render_to
 
 @login_required
@@ -28,18 +28,6 @@ def view(request, tracker_id):
     data = c.call.get_tracker_data(1)
     tracker.data = data
     return {'tracker': tracker, 'options': options}
-
-@login_required
-def add(request):
-    return form(request, TrackerModel(
-                                 #user=request.user
-                                 ))
-
-@login_required
-def edit(request, tracker_id):
-    return form(request, get_object_or_404(TrackerModel, pk=tracker_id,
-                                           #user=request.user
-                                           ))
 
 @csrf_protect
 @render_to('frontend/trackers/form.html')
