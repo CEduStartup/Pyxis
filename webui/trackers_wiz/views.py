5b87ba13e7c1a223e2d9255a6a64f59411cd0d02
Rando <randorando@gmail.com>
2011-11-02 18:09:48 +0200

    Removed needles statments

--- webui/trackers_wiz/views.py
+++ webui/trackers_wiz/views.py
@@ -14,27 +14,26 @@ def _make_initial(tracker):
     data_source = DataSourceModel.objects.get(tracker=tracker)
     value = ValueModel.objects.get(data_source=data_source)
 
-    method_name = ''
-    parms = ''
-    uri = ''
+    query = {}
     try:
         query = simplejson.loads(data_source.query)
-        method_name = query['method_name']
-        parms = query['parms']
-        uri = query['uri']
     except simplejson.decoder.JSONDecodeError:
         print 'Can not load json object'
-    finally:
-        return {
-             0: {'name': tracker.name, 'id': tracker.id,
-                 'status': tracker.status,
-                 'refresh_interval': tracker.refresh_interval},
-             1: {'data_type': data_source.data_type, 'parms': parms,
-                 'method_name': method_name, 'uri': uri,
-                 'access_method': data_source.access_method},
-             2: {'value_type': value.value_type , 'name': value.name,
-                 'extraction_rule': value.extraction_rule},
-           }
+
+    method_name = query.get('method_name', '')
+    parms = query.get('parms', '')
+    uri = query.get('uri', '')
+
+    return {
+         0: {'name': tracker.name, 'id': tracker.id,
+             'status': tracker.status,
+             'refresh_interval': tracker.refresh_interval},
+         1: {'data_type': data_source.data_type, 'parms': parms,
+             'method_name': method_name, 'uri': uri,
+             'access_method': data_source.access_method},
+         2: {'value_type': value.value_type , 'name': value.name,
+             'extraction_rule': value.extraction_rule},
+       }
 
 def edit(request, tracker_id):
     tracker = get_object_or_404(TrackerModel, pk=tracker_id)
