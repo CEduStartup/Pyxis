b4dd445ee2ff4696a280e90fba46475404ecdb51
ChakaBum <yura.senko@gmail.com>
2011-11-09 16:40:38 +0200

    event_dispatcher initialization added for collector process.

--- config/init/trackers.py
+++ config/init/trackers.py
@@ -1,3 +1,12 @@
+"""Creates and initialize components requred for trackers processing.
+"""
+
+from config.mq import queue_host, queue_port, COLLECTOR_TUBE
 from shared.events.EventManager import EventSender
+from shared.events.GEventDispatcher import GEventDispatcher
 
 sender = EventSender()
+
+# Events dispatcher object for collector.
+event_dispatcher = GEventDispatcher(queue_host, queue_port, (COLLECTOR_TUBE,))
+
