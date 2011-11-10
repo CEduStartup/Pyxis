5cf13de5d9f64e3cbfc17af1cdd4de22bee61fa5
ChakaBum <yura.senko@gmail.com>
2011-11-10 14:12:22 +0200

    Now webui generates event when new tracker added or tracker's config changed + tabs converted to spaces.

--- webui/trackers_wiz/forms.py
+++ webui/trackers_wiz/forms.py
@@ -7,10 +7,12 @@ from frontend.models import *
 
 from bootstrap.forms import BootstrapModelForm, Fieldset
 
+from shared.events.EventManager import EventSender
+
 class TrackerNameForm(BootstrapModelForm):
     class Meta:
         model = TrackerModel
-        fields = ('name', 'refresh_interval', 'status') 
+        fields = ('name', 'refresh_interval', 'status')
         layout = (
             Fieldset('General tracker information', 'name', 'refresh_interval', 'status', ),
         )
@@ -27,7 +29,7 @@ class DataSourceForm(BootstrapModelForm):
 class ValueForm(BootstrapModelForm):
     class Meta:
         model = ValueModel
-        fields = ('name', 'value_type', 'extraction_rule') 
+        fields = ('name', 'value_type', 'extraction_rule')
         layout = (
             Fieldset('Specify values location', 'name', 'value_type', 'extraction_rule', ),
         )
@@ -38,38 +40,55 @@ class TrackerWizard(FormWizard):
 
     def done(self, request, form_list):
         tracker = None
-	data_source = None
-	value = None
-	# Handle add vs edit cases.
-	if self.initial:
-	    edited_id = self.initial[0].get('id')
-	    if edited_id:
-	        tracker = get_object_or_404(TrackerModel, pk=edited_id)
+        data_source = None
+        value = None
+
+        is_new_tracker = False
+
+        # Handle add vs edit cases.
+        if self.initial:
+            edited_id = self.initial[0].get('id')
+            if edited_id:
+                tracker = get_object_or_404(TrackerModel, pk=edited_id)
+
         if not tracker:
-	    tracker = TrackerModel.objects.create(user=request.user)
+            tracker = TrackerModel.objects.create(user=request.user)
+            is_new_tracker = True
         else:
             data_source = DataSourceModel.objects.get(tracker=tracker)
-	if not data_source:
-	    data_source = DataSourceModel.objects.create(tracker=tracker)
-	else:
-	    value = ValueModel.objects.get(data_source=data_source)
-	if not value:
-	    value = ValueModel.objects.create(data_source=data_source)
-	value.data_source = data_source
-	data_source.tracker = tracker
-	# Fill in objects with new data.
-	tracker.name = form_list[0].cleaned_data['name']
-	tracker.refresh_interval = form_list[0].cleaned_data['refresh_interval']
-	tracker.status = form_list[0].cleaned_data['status']
-	data_source.access_method = form_list[1].cleaned_data['access_method']
-	data_source.query = form_list[1].cleaned_data['query']
-	data_source.data_type = form_list[1].cleaned_data['data_type']
-	value.name = form_list[2].cleaned_data['name']
-	value.value_type = form_list[2].cleaned_data['value_type']
-	value.extraction_rule = form_list[2].cleaned_data['extraction_rule']
-	# Save objects to database.
-	tracker.save()
-	data_source.save()
-	value.save()
+
+        if not data_source:
+            data_source = DataSourceModel.objects.create(tracker=tracker)
+        else:
+            value = ValueModel.objects.get(data_source=data_source)
+
+        if not value:
+            value = ValueModel.objects.create(data_source=data_source)
+
+        value.data_source = data_source
+        data_source.tracker = tracker
+        # Fill in objects with new data.
+        tracker.name = form_list[0].cleaned_data['name']
+        tracker.refresh_interval = form_list[0].cleaned_data['refresh_interval']
+        tracker.status = form_list[0].cleaned_data['status']
+        data_source.access_method = form_list[1].cleaned_data['access_method']
+        data_source.query = form_list[1].cleaned_data['query']
+        data_source.data_type = form_list[1].cleaned_data['data_type']
+        value.name = form_list[2].cleaned_data['name']
+        value.value_type = form_list[2].cleaned_data['value_type']
+        value.extraction_rule = form_list[2].cleaned_data['extraction_rule']
+        # Save objects to database.
+        tracker.save()
+        data_source.save()
+        value.save()
+
+        if is_new_tracker:
+            eid = 'CONFIG.TRACKER.ADDED'
+        else:
+            eid = 'CONFIG.TRACKER.CHANGED'
+
+        sender = EventSender()
+        sender.fire(eid, tracker_id=tracker.id)
+
         return HttpResponseRedirect('/trackers/')
 
