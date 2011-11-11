70d9cec0a78cec821bf56ecf7ffffd6c73a3fd5d
Evgen Koval <ekoval@ironport-training.lv.lohika.com>
2011-11-11 09:45:37 +0200

    Minor changes to generator.

--- testdata/generator/views.py
+++ testdata/generator/views.py
@@ -47,7 +47,7 @@ def html(request, function):
     next_value = calculate_next_value(function)
     result_html = html_template %{'value': next_value}
 #    gevent.sleep(random.random()*2)
-    time.sleep(random.random()*2)
+    time.sleep(random.random())
     return HttpResponse(result_html)
 
 
@@ -55,13 +55,13 @@ def xml(request, function):
     next_value = calculate_next_value(function)
     result_xml = xml_template %{'value': next_value}
 #    gevent.sleep(random.random()*2)
-    time.sleep(random.random()*2)
+    time.sleep(random.random())
     return HttpResponse(result_xml)
 
 def value(request, format, function, divider):
     next_value = calculate_next_value(function, int(divider or 100))
 #    gevent.sleep(random.random()*2)
-    time.sleep(random.random()*2)
+    time.sleep(random.random())
     return HttpResponse(
         TEMPLATES[format] % {'value': next_value}
     )
