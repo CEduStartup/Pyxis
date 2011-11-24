1bde474a24813aab323347c2641dd434a573aabb
Sasha B <obystrikov@ironport-training.lv.lohika.com>
2011-11-24 16:48:30 +0200

    Fixed user profile issue

--- webui/accounts/forms.py
+++ webui/accounts/forms.py
@@ -12,5 +12,5 @@ class UserProfileForm(Form):
         user.last_name = self.cleaned_data['last_name']
         user.save()
 
-    first_name  = CharField(max_length=60, required=False)
-    last_name  = CharField(max_length=60, required=False)
+    first_name  = CharField(max_length=60, required=True)
+    last_name  = CharField(max_length=60, required=True)
