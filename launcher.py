d819bd84b613d94861c2269ba56063ff124e48d1
Evgen Koval <ekoval@ironport-training.lv.lohika.com>
2011-11-29 16:49:03 +0200

    Now our BeansTalk-based events subsyste, used by Launcher to wait for dependent processes.

--- launcher.py
+++ launcher.py
@@ -1,70 +1,67 @@
 ## Run Pyxis.
 ##
 ## This program uses configuration from config/processes.py
-import argparse
 import gevent
-import os
-import subprocess
-import time
-from build_python_path import process_dir
-from config.services import launcher as launcher_config
 from gevent import monkey
-from services.service_base import SharedService
+monkey.patch_all()
+import subprocess
+import os
+from config.mq import queue_host, queue_port
+from build_python_path import process_dir
+from shared.events.GEventDispatcher import GEventDispatcher
+from shared.events.Event import ServiceChangedStateEvent, LAUNCHER_TUBE
 
 
+# Some processes have to inform Launcher that they're started and ready.
+# For example, collector depends on shared services. Dependent processes are
+# specified in process configuration (config/processes.py).
 ready_processes = []
 
+def on_service_state_event(event):
+    """This happens when some processes tells Launcher that it is ready."""
+    if event.state == 'started':
+        ready_processes.append(event.service_id)
 
-# This tiny BJsonRPC server, which receives notification from processes when
-# they're ready. We need, because we have dependent processes - for example,
-# Collector reuires shared services and logger.
-class LauncherInfo(SharedService):
-    config = launcher_config
-    # Methods your service export.
-    def _setup(self):
-        pass
 
-    def process_ready(self, pid):
-        ready_processes.append(pid)
+# Waiting for "Service Started" events.
+receiver = GEventDispatcher(queue_host, queue_port, (LAUNCHER_TUBE,))
+receiver.subscribe((ServiceChangedStateEvent,), on_service_state_event)
+event_thread = gevent.spawn(receiver.dispatch)
 
 
 def dependent_processes_ready(process):
+    """Checks if all dependent processes are ready.
+
+    :Parameters:
+        `process`: process instance (from config/processes.py)
+    """
     for p in process.depends_on:
         if not p.pid in ready_processes:
             return False
     return True
 
 
-def run_processes(debug=0):
-    threads = []
+def run_processes():
     executed_processes = []
     for process in processes:
         while(not dependent_processes_ready(process)):
             gevent.sleep(0.5)
-        process.set_debug(debug)
         cmd = [process.command]
         if isinstance(process.params, (list, tuple)):
             cmd.extend(process.params)
         else:
             cmd.append(process.params)
-        executed_processes.append(subprocess.Popen(cmd, cwd=process.cwd))
-        #threads.append(gevent.spawn(executed_processes[-1].wait))
-    return threads
+        subprocess.Popen(cmd, cwd=process.cwd)
 
 
 if __name__ == '__main__':
     os.environ['PYXIS_ROOT'] = os.getcwd()
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--debug', action='store_const', const=True,
-                        help='run system in debug mode')
-    args = parser.parse_args()
-    debug = args.debug or False
-
     from config.processes import processes
+
     pythonpath_dirs = process_dir(os.getcwd())
     pythonpath_dirs.append('%s/webui' %os.environ['PYXIS_ROOT'])
     os.environ['PYTHONPATH'] = ':'.join(pythonpath_dirs)
-    monkey.patch_all()
-    threads = [LauncherInfo.start()] + run_processes(debug)
-    gevent.joinall(threads)
+
+    run_processes()
+    while True:
+        gevent.sleep(0.01)
