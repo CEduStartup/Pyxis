14ab8ea1b18e30222ed382c17e191464a84319de
Evgen Koval <ekoval@ironport-training.lv.lohika.com>
2011-12-06 16:38:55 +0200

    Fixed wrong Django command file location.

--- webui/frontend/management/commands/dropalltrackers.py
+++ webui/frontend/management/commands/dropalltrackers.py
@@ -3,10 +3,14 @@
 from django.core.management.base import NoArgsCommand
 
 from frontend.models import TrackerModel
+from shared.services.services_api import mongo_storage_api
 
 
 class Command(NoArgsCommand):
     help = "Delete all trackers."
 
     def handle_noargs(self, **options):
-        TrackerModel.objects.delete_all_users()
+        for tracker in TrackerModel.objects.all():
+            tracker.delete()
+
+        mongo_storage_api().clear_db()
